836a1b7382dc487aec4593309c215aa90f997bed
Chris Oloff <christian.oloff@gmail.com>
2014-09-24 21:35:24 +0200

    fixed spec

--- test/spec/controllers/stuffs.js
+++ test/spec/controllers/stuffs.js
@@ -1,22 +1,22 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: StuffsCtrl', function () {
 
   // load the controller's module
   beforeEach(module('yapApp'));
 
-  var MainCtrl,
+  var StuffsCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    StuffsCtrl = $controller('StuffsCtrl', {
       $scope: scope
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(4);
+    expect(scope.awesomeThings.length).toBe(3);
   });
 });
