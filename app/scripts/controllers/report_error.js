e17029ff4939480bcfae36c35a07405b30ee94d6
Chris Oloff <christian.oloff@gmail.com>
2014-10-13 21:12:31 +0200

    improved error handling, adding date/time picker (wip)

--- app/scripts/controllers/report_error.js
+++ app/scripts/controllers/report_error.js
@@ -3,10 +3,19 @@
 angular.module('yapApp')
   .controller('ReportErrorCtrl', function ($scope, ErrorsService) {
     $scope.errors = [];
+    $scope.emailLink = "mailto:chris@uber5.com?subject=Error%20Report&body=" + 'testing body';
+
+    var errorEmailText = '';
+
     ErrorsService.errors().forEach(function(e) {
-      console.log('error', e);
       $scope.errors.push(JSON.stringify(e));
+      errorEmailText += 'Error: ' + e.message + '\n';
+      if (e.error.stack) {
+        errorEmailText += '  stack: ' + e.error.stack;
+      }
+      errorEmailText += '\n';
     });
+    $scope.emailBody = encodeURIComponent(errorEmailText);
     $scope.$on('$destroy', function() {
       ErrorsService.clear();
     });
